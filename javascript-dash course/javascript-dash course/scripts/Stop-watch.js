767337d8c28ef7e62063e5306f04c0868b436819
Andrey Shutyuk <andrewshoot@yandex.ru>
2023-06-15 15:05:06 +0300

    working with stopwatch learning

--- javascript-dash course/javascript-dash course/scripts/Stop-watch.js
+++ javascript-dash course/javascript-dash course/scripts/Stop-watch.js
@@ -1,8 +1,86 @@
 
-let second = 0;
-function timer () {
+let [seconds, minutes, hours] = [0, 0, 0];
+let timeRef = document.querySelector('.js-time');
+let timer = null;
+let button = document.querySelector('.js-stop-button');
+
+
+document.querySelector('.js-start-button')
+  .addEventListener('click', () => {
+    watchStart();
+  });
+
+document.querySelector('.js-stop-button')
+  .addEventListener('click', () => {
+    watchStop();
+});
+
+document.querySelector('.js-reset-button')
+  .addEventListener('click', () => {
+    watchReset();
+  });
+
+
+
+
+function stopwatch() {
+  seconds++;
+  if(seconds === 60) {
+    seconds = 0;
+    minutes++;
+  } else if(minutes === 60) {
+      minutes = 0;
+      hours++;
+  }
+
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  timeRef.innerHTML = h +":"+ m +":"+ s; 
+}
+
+function watchStart() {
+  
+  if(timer != null){
+    clearInterval(timer);
+  }
+    timer = setInterval(() => {
+    stopwatch();
+  }, 1000);
+  button.innerHTML = 'Stop';
+  button.classList.add('js-stop-button');
+  button.classList.remove('js-reset-button');
+}
+
+function watchStop() {
+  clearInterval(timer);
+}
+
+function watchReset() {
+  clearInterval(timer);
+  [seconds, minutes, hours] = [0, 0, 0];
+  timeRef.innerHTML = '00:00:00';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function timer () {
   second++;
   setTimeout(timer, 1000);
   console.log(second);
 }
-timer(); 
+timer(); */
